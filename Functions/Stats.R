496d6a74797fa0252d01ec9f46f2294f217b52bb
DavieCal <daviecalmail@gmail.com>
2016-11-23 21:48:10 +0000

    Toronto updates

--- Functions/Stats.R
+++ Functions/Stats.R
@@ -1,22 +1,86 @@
 #format scientific notation
 
 library(lme4)
-
+load("SpawnClust.RData")
+#select only september 
 data<-daily[daily$MONTH==9,]
+#select only males or females
+data<-daily[daily$MONTH==9 & (daily$SEX=="M" | daily$SEX=="F"),]
 
-#necessary to do likelihood ratio test
-fit.full<-lmer(sqrt(STEPLENGTH)~as.factor(SEX)+as.factor(YEAR)+(1|TRANSMITTER),data=data, REML=FALSE)
-fit.2<-lmer(sqrt(STEPLENGTH)~as.factor(SEX)+(1|TRANSMITTER),data=data, REML=FALSE)
-fit.1<-lmer(sqrt(STEPLENGTH)~as.factor(YEAR)+(1|TRANSMITTER),data=data, REML=FALSE)
-fit.null<-lmer(sqrt(STEPLENGTH)~1+(1|TRANSMITTER),data=data, REML=FALSE)
+library(fitdistrplus)
+library(car)
+library(MASS)
 
-fit.full.slope<-lmer(sqrt(STEPLENGTH)~as.factor(SEX)+as.factor(YEAR)+(1+YEAR|TRANSMITTER),data=data, REML=FALSE)
+y<-data$STEPLENGTH
+descdist(y)
+fit.norm<-fitdist(y,"norm")
+fit.gamma<-fitdist(y,"gamma")
+
+
+
+
+#Daily displacement
+qqp(data$STEPLENGTH,"gaussian")
+hist(sqrt(data$STEPLENGTH))
+
+
+boxplot(sqrt(STEPLENGTH)~SEX*YEAR,data)
+
+data$YEAR<-as.factor(data$YEAR)
+data$TRANSMITTER<-as.factor(data$TRANSMITTER)
+data$SEX<-as.factor(data$SEX)
+#REML= FALSE necessary to do likelihood ratio test
+fit.dd.full<-lmer(sqrt(STEPLENGTH)~SEX+(1|TRANSMITTER)+(1|YEAR),data=data, REML=FALSE)
+fit.dd.null<-lmer(sqrt(STEPLENGTH)~1+(1|TRANSMITTER)+(1|YEAR),data=data, REML=FALSE)
+
+anova(fit.dd.full,fit.dd.null)
+qqnorm(resid(fit.dd.full))
+summary(fit.dd.full)
 #look at residuals
-plot(fit.full.slope)
+plot(fit.dd.full)
 #normality of residuals
-hist(residuals(fit.full.slope))
-fit<-lm(sqrt(STEPLENGTH)~as.factor(SEX)+as.factor(YEAR),data=data)
+hist(residuals(fit.dd.full))
 
-boxplot(STEPLENGTH~YEAR*SEX,data)
-head(dfbeta(fit))
+coef(fit.dd.full)
+
+
+
+#Persistence index
+
+hist(data$PERSINDEX)
+
+boxplot(PERSINDEX~SEX*YEAR,data)
+
+fit.pi.full<-lmer(PERSINDEX~SEX+(1|TRANSMITTER)+(1|YEAR),data=data, REML=FALSE)
+fit.pi.null<-lmer(PERSINDEX~1+(1|TRANSMITTER)+(1|YEAR),data=data, REML=FALSE)
+anova(fit.pi.full,fit.pi.null)
+summary(fit.pi.full)
+hist(residuals(fit.pi.full))
+
+#Acceleration
+
+data.ac<-data[data$YEAR!=2012,]
+levels(data.ac$YEAR)<-c("2013","2014")
+hist(log(data.ac$ACCEL))
+boxplot(ACCEL~SEX*YEAR,data.ac)
+
+
+fit.ac.full<-lmer(log(ACCEL)~SEX+YEAR+(1|TRANSMITTER),data=data.ac, REML=FALSE)
+fit.ac.sex<-lmer(log(ACCEL)~SEX+(1|TRANSMITTER),data=data.ac, REML=FALSE)
+fit.ac.year<-lmer(log(ACCEL)~YEAR+(1|TRANSMITTER),data=data.ac, REML=FALSE)
+fit.ac.null<-lmer(log(ACCEL)~(1|TRANSMITTER),data=data.ac, REML=FALSE)
+
+anova(fit.ac.full,fit.ac.sex)
+anova(fit.ac.full,fit.ac.year)
+anova(fit.ac.full,fit.ac.sex)
+summary(fit.ac.sex)
+plot(fit.ac.full)
+hist(residuals(fit.ac.full))
+
+coef(fit.ac.full)
+
+############################
+
+
+
 
